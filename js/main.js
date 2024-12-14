229fd074289d35b75f86ef007dc4388a14910815
KashifKhn <kashifkhnx04@gmail.com>
2024-12-14 12:58:00 +0500

    make the left paddel move with arrow keys also bound to limit of canvas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log("pingpong Game by KashifKhn");
-
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -11,6 +9,8 @@ const PADDLE_WIDTH = 10;
 
 let leftPaddleY = CANVAS_HEIGHT / 2 - PADDLE_HEIGHT / 2;
 let rightPaddleY = CANVAS_HEIGHT / 2 - PADDLE_HEIGHT / 2;
+let paddleSpeed = 0;
+let intervalId = null;
 
 ctx.fillStyle = "white";
 ctx.fillRect(0, leftPaddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
@@ -20,3 +20,34 @@ ctx.fillRect(
   PADDLE_WIDTH,
   PADDLE_HEIGHT,
 );
+
+document.addEventListener("keydown", handleKeydow);
+document.addEventListener("keyup", handleKeyup);
+
+function handleKeydow(event) {
+  if (event.key === "ArrowUp") {
+    paddleSpeed = -5;
+  }
+  if (event.key === "ArrowDown") {
+    paddleSpeed = 5;
+  }
+}
+
+function updateGameState() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  ctx.fillStyle = "white";
+  leftPaddleY = Math.max(
+    0,
+    Math.min(leftPaddleY + paddleSpeed, CANVAS_HEIGHT - PADDLE_HEIGHT),
+  );
+  ctx.fillRect(0, leftPaddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
+}
+
+function handleKeyup(event) {
+  if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+    paddleSpeed = 0;
+  }
+}
+
+setInterval(updateGameState, 8);
